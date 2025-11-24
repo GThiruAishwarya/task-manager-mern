5bce77fe8f901f402fbf1944e6411ba512852767
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-11-24 07:21:03 +0530

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+require('dotenv').config();
+
+
+module.exports = async function(req, res, next) {
+const token = req.header('Authorization')?.replace('Bearer ', '') || req.header('x-auth-token');
+if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+try {
+const decoded = jwt.verify(token, process.env.JWT_SECRET);
+req.user = await User.findById(decoded.id).select('-password');
+if (!req.user) return res.status(401).json({ message: 'Invalid token (user not found)' });
+next();
+} catch (err) {
+return res.status(401).json({ message: 'Token is not valid' });
+}
+};
